efc5ab490788751d9ad49113f7ac16d01ce5e3ed
vinihzets <viniciusdaarky@outlook.com>
2023-04-11 07:56:35 -0300

    Criando bloc_builder global pra utilizacao em toda aplicacao

---
+++ lib/core/architeture/bloc_builder.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:youtube_app/core/architeture/bloc_state.dart';
+
+class BlocScreenBuilder extends StatelessWidget {
+  final Stream<BlocState> stream;
+  final Widget Function(BlocState) builder;
+  const BlocScreenBuilder(
+      {required this.stream, required this.builder, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: stream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return builder(snapshot.data!);
+          }
+          return const SizedBox.shrink();
+        });
+  }
+}
